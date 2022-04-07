4db25adc589c92151e34642eddbfffc89cf4b7e9
Ayooluwa Isaiah <ayisaiah@gmail.com>
2022-04-07 21:03:56 +0100

    Move some functions to utils file

--- src/internal/stats.go
+++ src/internal/stats.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"math"
 	"sort"
 	"strings"
 	"time"
@@ -890,30 +889,4 @@ func NewStats(ctx statsCtx, store DB) (*Stats, error) {
 	}
 
 	return s, nil
-}
-
-// roundTime rounds a time value in seconds, minutes, or hours to the nearest integer.
-func roundTime(t float64) int {
-	return int(math.Round(t))
-}
-
-// minsToHoursAndMins expresses a minutes value
-// in hours and mins.
-func minsToHoursAndMins(val int) (hrs, mins int) {
-	hrs = int(math.Floor(float64(val) / float64(minutesInAnHour)))
-	mins = val % minutesInAnHour
-
-	return
-}
-
-// contains checks if a string is present in
-// a string slice.
-func contains(s []timePeriod, e timePeriod) bool {
-	for _, a := range s {
-		if a == e {
-			return true
-		}
-	}
-
-	return false
 }
